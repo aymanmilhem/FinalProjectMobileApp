83349577ceb97714bfbac4284d4cfe4d257a374a
Ayman Milhem <aymanmilhem@gmail.com>
2019-12-02 14:28:47 +1300

    * set up sql-net-pcl properly * solve a blockage

---
+++ GreenWorld/GreenWorld.Android/Persistence/SQLiteDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GreenWorld.Persistence;
+using SQLite;
+using Xamarin.Forms;
+using Environment = System.Environment;
+
+
+namespace GreenWorld.Droid.Persistence
+{
+    class SQLiteDB : ISQLiteDB
+    {
+        
+        public  SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return  new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ GreenWorld/GreenWorld/Persistence/ISQLiteDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace GreenWorld.Persistence
+{
+    public interface ISQLiteDB
+    {
+        // Had to install sql-net-pcl-Async for
+        // this to work
+        // ran into major issue and turned back
+        //SQLiteAsyncConnection GetConnection();
+
+        //SQLiteConnection GetConnection();
+        /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+        // Attention: all the above confusion because 
+        // of installing the wrong sqlite-net-pcl package
+        // there are multiple and should install only this one!!
+        SQLiteAsyncConnection GetConnection();
+
+    }
+}
